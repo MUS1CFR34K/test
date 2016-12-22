0252e5dbe6702a504cbc9c87840e499d344b9a81
MUS1CFR34K <david.baligac@gmail.com>
2016-12-22 16:24:06 +0100

    Add files via upload

---
+++ naloga14.cpp
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <iostream>	
+#include <string>
+#include <fstream> 
+
+			
+using namespace std;
+
+ string naziv;
+ int kolicina;
+ int cena;
+ int zaporedna_stevilka;
+int a=cena;
+int b=kolicina;
+int vrednost;
+
+int main(){
+
+
+ ofstream myfile;
+ myfile.open ("evidenca.txt");
+ getline 
+ myfile.open ("katalog.txt");
+ myfile.close();
+
+ return 0;
+}
